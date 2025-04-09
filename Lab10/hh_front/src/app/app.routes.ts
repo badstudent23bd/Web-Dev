a593d53701021f4d24c7df2fb7e86c4f30975154
inara <i_assylzhanova@kbtu.kz>
2025-04-09 14:29:11 +0500

    Delete Lab10 directory

--- Lab10/hh_front/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { CompanyListComponent } from './features/company/company-list/company-list.component';
-import { VacancyListComponent } from './features/vacancy/vacancy-list/vacancy-list.component';
-
-export const routes: Routes = [
-  { path: '', redirectTo: 'companies', pathMatch: 'full' },
-  { path: 'companies', component: CompanyListComponent },
-  { path: 'companies/:id/vacancies', component: VacancyListComponent },
-];
